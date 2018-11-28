e84fd441b4790436d339ee8444a490b64d5e0a41
Vinicius Teixeira <viniciuslt@outlook.com>
2018-11-27 22:37:51 -0200

    Projeto

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject codigos-tcc "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject BMH-IG "PFSP"
+  :description "Descrição: Algoritmos BMH e IG_BLSP"
+  :url "https://github.com/ViniciusTxr/algoritmos-flowshop"
+  :license {:name "Public License"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot core
   :repl-options {:timeout 2000000}
